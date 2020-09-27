2fbc19f5f7f46674b7fad40066c07043907f1e19
asadsanto10 <santo.meridian@gmail.com>
2020-09-27 22:06:27 +0600

    slider added

--- js/main.js
+++ js/main.js
@@ -7,4 +7,68 @@ $(document).ready(function () {
             $('#header').removeClass('scroll');
         }
     });
+    
+    /*=============silk slider=====================*/
+    $('#testimonila-slider #slider').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        //dots: true,
+        prevArrow: false,
+        nextArrow: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        prevArrow: "<img class='a-left control-c prev slick-prev' src='../image/Desktop/icon/left-arrow.png'>",
+        nextArrow: "<img class='a-right control-c next slick-next' src='../image/Desktop/icon/right-arrow.png'>",
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        }
+        ]
+    });
+    $('#video-testimonila #video-testimonila-slider').slick({
+        infinite: true,
+        autoplay: false,
+        // autoplaySpeed: 3000,
+        //dots: true,
+        prevArrow: false,
+        nextArrow: false,
+        speed: 300,
+        slidesToShow: 1,
+        rows: 4,
+        slidesToScroll: 1,
+        nextArrow: "<img class='a-right control-c next slick-next' src='../image/Desktop/icon/next (1).png'>",
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                rows: 2,
+                infinite: true,
+            }
+        }
+        ]
+    });
+    /*=============silk slider=====================*/
 })
