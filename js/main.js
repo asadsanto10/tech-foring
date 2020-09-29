c815848827bb4f627227cc99bed9fb5be9512216
asadsanto10 <santo.meridian@gmail.com>
2020-09-29 23:28:29 +0600

    change

--- js/main.js
+++ js/main.js
@@ -9,13 +9,13 @@ $(document).ready(function () {
     });
     
     /*=============silk slider=====================*/
+
+    // testimonila slider
     $('#testimonila-slider #slider').slick({
         infinite: true,
         autoplay: true,
         autoplaySpeed: 3000,
         //dots: true,
-        prevArrow: false,
-        nextArrow: false,
         speed: 300,
         slidesToShow: 5,
         slidesToScroll: 2,
@@ -47,6 +47,8 @@ $(document).ready(function () {
         }
         ]
     });
+
+    // video-testimonila slider
     $('#video-testimonila #video-testimonila-slider').slick({
         infinite: true,
         autoplay: false,
@@ -65,6 +67,41 @@ $(document).ready(function () {
             settings: {
                 slidesToShow: 1,
                 rows: 2,
+                infinite: true,
+            }
+        }
+        ]
+    });
+    // feedback slider
+    $('#feedback #single-feedback').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        prevArrow: false,
+        nextArrow: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+    
+    //client - slider
+    $('#single-client-slider').slick({
+        infinite: true,
+        // autoplay: true,
+        autoplaySpeed: 3000,
+        // dots: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        prevArrow: "<img class='a-left control-c prev slick-prev' src='./image/Desktop/icon/prev.png'>",
+        nextArrow: "<img class='a-right control-c next slick-next' src='./image/Desktop/icon/next.png'>",
+        responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 infinite: true,
             }
         }
